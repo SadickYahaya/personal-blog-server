16a5a2bb9c9a5980f79d8d6e49c2b41d5bc90f89
Abubakar Yahaya <abubakar.yahaya@amalitech.com>
2024-09-30 21:59:46 +0000

    modifying cors to add frontend url

--- server.js
+++ server.js
@@ -9,7 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['https://sadickyahaya.netlify.app'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use(bodyParser.json());
 
 // Connect to MongoDB
