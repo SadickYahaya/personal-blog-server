f589528fabd33d1599b966ce1e4eb7fd50bae980
Abubakar Yahaya <abubakar.yahaya@amalitech.com>
2024-10-01 20:07:34 +0000

    production db

--- server.js
+++ server.js
@@ -7,10 +7,13 @@ require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+const MONGO_URL = process.env.NODE_ENV === 'production' 
+  ? process.env.MONGO_URL_PROD 
+  : process.env.MONGO_URL_DEV;
 
 // Middleware
 app.use(cors({
-  origin: ['https://sadickyahaya.netlify.app'],
+  origin: ['https://sadickyahaya.netlify.app', 'http://localhost:5173'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
@@ -18,7 +21,7 @@ app.use(cors({
 app.use(bodyParser.json());
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
