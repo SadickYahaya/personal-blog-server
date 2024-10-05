925fbeb2305cf3a76458ef962d29e25eaafbdec2
Abubakar Yahaya <abubakar.yahaya@amalitech.com>
2024-10-05 00:37:49 +0000

    posting to twitter

--- routes/blogPosts.js
+++ routes/blogPosts.js
@@ -8,6 +8,7 @@ const createTransporter = require('../helpers/emailService');
 const { upload } = require('../helpers/imageUpload');
 const QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
 const Comment = require('../models/Comment'); 
+const { TwitterApi } = require('twitter-api-v2');
 
 // Function to send newsletter emails
 async function sendNewsletterEmails(blogPost) {
@@ -87,7 +88,56 @@ async function sendNewsletterEmails(blogPost) {
   }
 }
 
-// Create a new blog post
+async function postToTwitter(blogPost) {
+  const client = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  });
+
+  // Extract a plain text excerpt from the description
+  let descriptionExcerpt = '';
+  try {
+    const descriptionContent = JSON.parse(blogPost.description);
+    descriptionExcerpt = descriptionContent.ops.map(op => op.insert || '').join('').trim();
+  } catch (error) {
+    console.error('Error parsing description:', error);
+    descriptionExcerpt = 'Check out my latest blog post!';
+  }
+
+  const blogUrl = `${process.env.FRONTEND_URL}/blog-details/${blogPost._id}`;
+  
+  // Craft the tweet
+  const maxTweetLength = 280;
+  const ellipsis = '...';
+  const readMore = 'Read more:';
+  const separator = '\n\n';
+
+  let tweet = `${blogPost.title}${separator}`;
+  const remainingLength = maxTweetLength - tweet.length - blogUrl.length - readMore.length - 2 * separator.length - ellipsis.length;
+
+  if (descriptionExcerpt.length > remainingLength) {
+    descriptionExcerpt = descriptionExcerpt.substring(0, remainingLength).trim() + ellipsis;
+  }
+
+  tweet += `${descriptionExcerpt}${separator}${readMore} ${blogUrl}`;
+
+  console.log('Attempting to post tweet:', tweet);
+
+  try {
+    const response = await client.v2.tweet(tweet);
+    console.log('Posted to Twitter:', response);
+    return response;
+  } catch (error) {
+    console.error('Error posting to Twitter:', error);
+    if (error.data) {
+      console.error('Twitter API Error Data:', JSON.stringify(error.data, null, 2));
+    }
+    throw error;
+  }
+}
+
 router.post('/', upload.single('image'), async (req, res) => {
   try {
     console.log('Received file:', req.file);
@@ -109,8 +159,14 @@ router.post('/', upload.single('image'), async (req, res) => {
     await blogPost.save();
     console.log('Saved blog post:', blogPost);
     
-    // Send newsletter emails
     await sendNewsletterEmails(blogPost);
+    
+    try {
+      await postToTwitter(blogPost);
+      console.log('Posted to Twitter');
+    } catch (error) {
+      console.error('Error posting to Twitter:', error);
+    }
     
     res.status(201).json(blogPost);
   } catch (err) {
