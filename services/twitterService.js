79d2c1aa9da5bd881fc04ec70c022d322d128cf8
Abubakar Yahaya <abubakar.yahaya@amalitech.com>
2024-10-08 22:07:31 +0000

    twitterr post revamp

--- services/twitterService.js
+++ services/twitterService.js
@@ -20,20 +20,40 @@ async function postToTwitter(blogPost) {
 
   const blogUrl = `${process.env.FRONTEND_URL}/blog-details/${blogPost._id}`;
   
-  // Craft the tweet
+  // Craft an engaging tweet
   const maxTweetLength = 280;
   const ellipsis = '...';
-  const readMore = 'Read more:';
+  const callToAction = 'Check it out:';
   const separator = '\n\n';
 
-  let tweet = `${blogPost.title}${separator}`;
-  const remainingLength = maxTweetLength - tweet.length - blogUrl.length - readMore.length - 2 * separator.length - ellipsis.length;
+  // Create an array of catchy openers
+  const openers = [
+    "🚀 New blog post alert!",
+    "📝 Fresh insights:",
+    "🔥 Hot off the press:",
+    "💡 Thought-provoking read:",
+    "🌟 You won't want to miss this:",
+  ];
+
+  // Randomly select an opener
+  const opener = openers[Math.floor(Math.random() * openers.length)];
+
+  let tweet = `${opener} "${blogPost.title}"${separator}`;
+  const remainingLength = maxTweetLength - tweet.length - blogUrl.length - callToAction.length - 2 * separator.length - ellipsis.length;
 
   if (descriptionExcerpt.length > remainingLength) {
-    descriptionExcerpt = descriptionExcerpt.substring(0, remainingLength).trim() + ellipsis;
+    // Find the last space within the remaining length to avoid cutting words
+    const lastSpaceIndex = descriptionExcerpt.lastIndexOf(' ', remainingLength);
+    descriptionExcerpt = descriptionExcerpt.substring(0, lastSpaceIndex > 0 ? lastSpaceIndex : remainingLength).trim() + ellipsis;
   }
 
-  tweet += `${descriptionExcerpt}${separator}${readMore} ${blogUrl}`;
+  tweet += `${descriptionExcerpt}${separator}${callToAction} ${blogUrl}`;
+
+  // Add relevant hashtags if there's room
+  const hashtags = ' #blog #tech #mustread';
+  if (tweet.length + hashtags.length <= maxTweetLength) {
+    tweet += hashtags;
+  }
 
   console.log('Attempting to post tweet:', tweet);
 
